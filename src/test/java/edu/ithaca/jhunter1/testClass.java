7877a75149ffdecd150639acebd9e6a76fb078c2
John Hunter <jhunter1@ithaca.edu>
2019-02-21 19:24:13 -0500

    First Commit

---
+++ src/test/java/edu/ithaca/jhunter1/testClass.java
@@ -0,0 +1,10 @@
+package edu.ithaca.jhunter1;
+
+import org.junit.Test;
+
+public class testClass {
+    @Test
+    public void getEmailTest() {
+        System.out.println("Hello world!");
+    }
+}
